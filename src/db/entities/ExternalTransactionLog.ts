398f4adde421b35a1e89d2898fe53d4e808acb5c
Amir Zametica <amir.zametica@klika.ba>
2021-11-16 11:27:34 +0100

    Extend external transaction log (#14)

--- src/db/entities/ExternalTransactionLog.ts
+++ src/db/entities/ExternalTransactionLog.ts
@@ -1,5 +1,17 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { ExternalTransactionBase } from './ExternalTransactionBase';
 
 @Entity()
-export class ExternalTransactionLog extends ExternalTransactionBase {}
+export class ExternalTransactionLog extends ExternalTransactionBase {
+  @Column({ nullable: true })
+  symbol: string;
+
+  @Column({ nullable: true })
+  sender: string;
+
+  @Column({ nullable: true })
+  receiver: string;
+
+  @Column({ type: 'decimal', nullable: true })
+  amount: string;
+}
