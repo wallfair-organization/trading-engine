d050abf13bb3f45db92ad185cc64ea1da1efac83
Stefan Hess <mail@stefanhess.ch>
2021-11-29 21:21:38 +0700

    fix: don't reuse default connection

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -2,14 +2,6 @@ import { createConnection, getConnectionManager } from 'typeorm';
 import config from '../db/config';
 
 export const initDb = async (dbConfig = null) => {
-  if (getConnectionManager().has('default')) {
-    const connection = getConnectionManager().get('default');
-    if (!connection.isConnected) {
-      await connection.connect();
-    }
-    return connection;
-  }
-
   return await createConnection(dbConfig ? dbConfig : config);
 };
 
