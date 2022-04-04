472a0908f2018c8ca554a25a16c36c84f03e3b86
Amir Zametica - External <amir.zametica@klika.ba>
2022-04-04 12:01:17 +0200

    WFAIR-1110: Add method to get sum amount by originator

--- src/lib/modules/transactions.ts
+++ src/lib/modules/transactions.ts
@@ -233,4 +233,20 @@ export class Transactions extends BaseModule {
       },
     });
   }
+
+  async getSumAmountByOriginator(
+    originator: ExternalTransactionOriginator,
+    userId: string
+  ) {
+    const result = await this.entityManager
+      .createQueryBuilder()
+      .select('SUM(amount)')
+      .from(ExternalTransactionLog, 'log')
+      .where({
+        originator,
+        internal_user_id: userId,
+      })
+      .getRawOne();
+    return result.sum || 0;
+  }
 }
